5a8b498b8c0d8691affda50eadbfd9c809aefef3
Greg Banks <gbanks@linkedin.com>
2020-04-18 22:23:40 -0400

    Initial version of a libiberty formula, with no bottles

---
+++ Formula/libiberty.rb
@@ -0,0 +1,67 @@
+#
+#  Copyright 2020 Gregory Banks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# Formula for libiberty, based on
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/binutils.rb
+#
+# For reasons that make best sense to them, the Homebrew maintainers have
+# decided not to ship a libiberty.a library binary or libiberty/*.h include
+# files with their binutils package.  This contrasts with the approach
+# chosen independently by multiple packagers in the Linux world, all of
+# whom package those files in their binutils-dev or binutils-devel package.
+# It also means that Homebrew users have no convenient means of installing
+# libiberty to enable building of software that depends on it, usually via
+# libbfd.  NovaProva is one such software, so to build NovaProva on MacOS
+# we need to provide our own libiberty.
+#
+class Libiberty < Formula
+  desc "GNU libiberty library for native development"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz"
+  sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
+
+  # TODO: bottles
+#  bottle do
+#    sha256 "1e21593a927df65e405f9d3bdc8f86fe83b1236c5c945641a2de775c99327953" => :catalina
+#    sha256 "142e380448ac77bc0f7974ff9b9ddae6a90c4ef5f182cac0c2b029baa8460173" => :mojave
+#    sha256 "87bda0c909a5bd2043d35b073f2268cac7aed074a89d903973e4909d68dfdf46" => :high_sierra
+#  end
+
+  keg_only :shadowed_by_macos, "Apple's CLT provides the same tools"
+
+  uses_from_macos "zlib"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-deterministic-archives",
+                          "--prefix=#{prefix}",
+                          "--infodir=#{info}",
+                          "--mandir=#{man}",
+                          "--disable-werror",
+                          "--enable-interwork",
+                          "--enable-multilib",
+                          "--enable-64-bit-bfd",
+                          "--enable-targets=all",
+                          "--enable-install-libiberty"
+    system "make", "all-libiberty"
+    system "make", "-C", "libiberty", "MULTIOSDIR=", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# homebrew-novaprova
-Homebrew tap for installing NovaProva and some of its dependencies
+# Novaprova Novaprova
+
+## How do I install these formulae?
+`brew install novaprova/novaprova/<formula>`
+
+Or `brew tap novaprova/novaprova` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/novaprova/homebrew-novaprova/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
